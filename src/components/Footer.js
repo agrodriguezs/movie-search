273dd2763190761c6eacd2fb1a2a30ae886cb8c6
Adriana Rodriguez Sanabria <agrodriguezs09@gmail.com>
2020-04-26 13:04:15 +0200

    cambios en el footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,14 @@ import React from 'react';
 export const Footer = () => (
   <footer className="pie">
     <div className="content">
-      <p>by
-         <a href="https://agrodriguezs.github.io/"  target="_blank">
-         Adriana Rodriguez</a>.<br />
-         Done with 
-         <a href="https://es.reactjs.org/" target="_blank">
-         React.js
-         </a> using <a href="http://www.omdbapi.com/" target="_blank">
-         OMDb API</a>. Udemy Course Project
-         <a href="https://www.udemy.com/course/aprendiendo-react/" target="_blank">
-         "Aprender React JS"</a></p>
+      <div className="parrafo" >By <a href="https://agrodriguezs.github.io/"  target="_blank">
+         Adriana Rodriguez.</a></div>
+      <div className="parrafo" >Done with <a href="https://es.reactjs.org/" target="_blank">React JS.        </a>
+      using <a href="http://www.omdbapi.com/" target="_blank">
+         OMDb API.</a> </div>
+         <div className="parrafo" >Udemy Course Project
+        <a href="https://www.udemy.com/course/aprendiendo-react/" target="_blank">
+         "Aprender React JS"</a></div>
     </div>
   </footer>
 
