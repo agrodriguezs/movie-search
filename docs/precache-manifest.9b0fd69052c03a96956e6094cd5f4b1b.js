edc60ad3287e420a54b3661e837281c728bcc1a0
Adriana Rodriguez Sanabria <agrodriguezs09@gmail.com>
2020-05-12 21:50:25 +0200

    site

---
+++ docs/precache-manifest.9b0fd69052c03a96956e6094cd5f4b1b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9ac838780d37d3a5f7036f20483661",
+    "url": "/index.html"
+  },
+  {
+    "revision": "936e2d890eaab28045cd",
+    "url": "/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "db18a0ac4b9e64ff5cd9",
+    "url": "/static/css/main.6b7da3b2.chunk.css"
+  },
+  {
+    "revision": "936e2d890eaab28045cd",
+    "url": "/static/js/2.a7e33082.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a7e33082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db18a0ac4b9e64ff5cd9",
+    "url": "/static/js/main.e65027bb.chunk.js"
+  },
+  {
+    "revision": "cd876ec1cb1e91b39625",
+    "url": "/static/js/runtime-main.2d185d26.js"
+  },
+  {
+    "revision": "cba8511e56e159dba6fe8986dccf1fd2",
+    "url": "/static/media/cinema.cba8511e.png"
+  },
+  {
+    "revision": "2b78e297833b62365b7e5ffcceecbaeb",
+    "url": "/static/media/imgNoFound.2b78e297.jpg"
+  },
+  {
+    "revision": "f0033b9edfe9c88727c54f815de52bb9",
+    "url": "/static/media/nofound.f0033b9e.png"
+  }
+]);
